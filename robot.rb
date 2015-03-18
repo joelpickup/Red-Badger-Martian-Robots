ebfbaf87a083c40a162c5f894f8cf29e8ea16594
Joel Pickup <joelpickup@hotmail.co.uk>
2015-03-18 18:53:49 +0000

    added movement methods to robot

--- robot.rb
+++ robot.rb
@@ -11,4 +11,54 @@ class Robot
     self.status = "DEPLOYED"
   end
 
+  def move(instructions)
+    instructions.each do |instruction|
+      case instruction
+      when 'L'
+        turn_left
+      when 'R'
+        turn_right
+      when 'F'
+        move_forward
+      end
+    end
+  end
+
+  def turn_left
+    case orientation
+    when 'N'
+      orientation = 'W'
+    when 'W'
+      orientation = 'S'
+    when 'S'
+      orientation = 'E'
+    when 'E'
+      orientation = 'N'
+    end
+  end
+
+  def turn_right
+    case orientation
+    when 'N'
+      orientation = 'E'
+    when 'E'
+      orientation = 'S'
+    when 'S'
+      orientation = 'W'
+    when 'W'
+      orientation = 'N'
+    end
+  end
+
+  def move_forward
+    case orientation
+    when 'N'
+      x_coord += 1
+    when 'S'
+      x_coord -= 1
+    when 'E'
+      x_coord += 1
+    when 'W'
+      x_coord -= 1 
+  end
 end
